65170ccf8bee90fc4f7794c00392850e064c2c7a
dineshkr01 <aec.cse.dinesh@gmail.com>
2025-11-06 23:45:35 +0530

    Delete main/main directory

--- main/main/java/com/example/charityManagementSystem/rough_codes/PasswordVerificationTokenRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.charityManagementSystem.rough_codes;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface PasswordVerificationTokenRepo extends JpaRepository<PasswordVerificationToken, Long>{
-
-}
--- main/main/java/com/example/charityManagementSystem/rough_codes/VerificationTokenRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.charityManagementSystem.rough_codes;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface VerificationTokenRepo extends JpaRepository<VerificationToken, Long>{
-
-}
